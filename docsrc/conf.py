786f937d83608d58496f91d6808aa26e7036cd7d
phiarchitect <phi@phiarchitect.com>
2024-05-05 16:00:14 -0700

    update website

---
+++ docsrc/conf.py
@@ -0,0 +1,13 @@
+#  from ablog.conf import *
+from photon_platform.sphinxilator.global_conf import *
+import photon_platform.modulator as module
+
+version = module.__version__
+
+org = "photon-platform"
+org_name = "photon-platform"
+
+repo = "modulator"
+repo_name = "modulator"
+
+setup_globals(org, org_name, repo, repo_name)
